029cbd8a1762cd633bf2c1496d4c156c96ee6143
Amoy Nicholson <moyanich@users.noreply.github.com>
2019-11-16 08:56:37 -0500

    Update strings.py

--- strings.py
+++ strings.py
@@ -3,6 +3,19 @@
 string_exp = "Tomato"
 print(string_exp)
 
-# Printing a index
+# Printing an index
 exp_2 = "Dance Moms"
 print(exp_2[2])
+print("apple"[4])
+
+# String Slicing
+exp_3 = "Tomato"
+
+# Slicing a string from the beginning to an end point
+print(exp_3[:3])
+
+# Slicing a string from a starting point to an end point
+print(exp_3[2:5])
+
+# Slicing a string from a starting point to the end of string
+print(exp_3[2:])
