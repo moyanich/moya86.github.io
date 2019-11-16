d30d417e9f401bc307f101573061c4c464dc2b51
Amoy Nicholson <moyanich@users.noreply.github.com>
2019-11-16 09:25:19 -0500

    Update strings.py
    Examples of String concatenation and string slicing

--- strings.py
+++ strings.py
@@ -19,3 +19,40 @@ print(exp_3[2:5])
 
 # Slicing a string from a starting point to the end of string
 print(exp_3[2:])
+
+
+# STRING CONCATENATION
+# Using the plus(+) operator
+print("Good" + " " + "morning")
+
+concatenated_string = "evening prim" + "-" + "rose"
+print(concatenated_string)
+print(concatenated_string[4])
+print(concatenated_string[1:4])
+
+
+example = "Just do it!"
+print(example[10])
+print(example[5:7])
+print(example[8:])
+print(example[:4])
+print(example[5:7] + " " + example[8:])
+print("Don't" + " " + example[5:7] + " " + example[8:])
+
+# Refactored
+
+sentence = "Just do it!"
+
+sentence_slided = sentence[10]
+print(sentence_slided)
+
+sentence_slided_2 = sentence[5:7]
+print(sentence_slided_2)
+
+sentence_slided_3 = sentence[8:]
+print(sentence_slided_3)
+
+sentence_slided_4 = sentence[:4]
+print(sentence_slided_4)
+
+print("Don't" + " " + sentence_slided_2 + " " + sentence_slided_3)
