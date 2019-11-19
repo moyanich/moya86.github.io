dcbbf319da686d2f1cfbeecaecafa116db2090eb
Amoy Nicholson <moyanich@users.noreply.github.com>
2019-11-19 09:40:24 -0500

    Escape sequences example

---
+++ escape_sequences.py
@@ -0,0 +1,22 @@
+# Escape Sequences
+# E.G:
+# Using your knowledge of escape sequences,
+# create a single print() statement with single string inside of it that
+# displays an upside down tree when the program is run:
+# see @ https://linuxconfig.org/list-of-python-escape-sequence-characters-with-examples
+#
+
+# New line ''\n
+print("First Line\nSecond Line")
+print("\n")
+print("********\n *****\n  ***\n   *")
+
+
+# Output
+# First Line
+# Second Line
+#
+# ********
+#  *****
+#   ***
+#    *
