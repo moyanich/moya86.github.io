3d15616605fa93250bbe4adf36350a7509cbed8e
Amoy N <moyanich@users.noreply.github.com>
2019-12-31 09:28:19 -0500

    Update conditionals.py
    Hackerrank solution to IF-Else question

--- conditionals.py
+++ conditionals.py
@@ -1,14 +1,31 @@
+# Python If-Else
+# Source: Hackerrank
 # Given an integer, , perform the following conditional actions:
 
-# If  is odd, print Weird
-# If  is even and in the inclusive range of  to , print Not Weird
-# If  is even and in the inclusive range of  to , print Weird
-# If  is even and greater than , print Not Weird
+# If n is odd, print Weird
+# If n is even and in the inclusive range of 2 to 5, print Not Weird
+# If n is even and in the inclusive range of 6 to 20, print Weird
+# If n is even and greater than 20, print Not Weird
 # Input Format
-
 # A single line containing a positive integer, n.
 
-n = input("Type word")
 
-if n=="Weird":
+n = int(input("Enter a number: "))
+
+if n % 2 == 1:
     print("Weird")
+elif n % 2 == 0 and 2 <= n <= 5:
+    print("Not Weird")
+elif n % 2 == 0 and 6 <= n <= 20:
+    print("Weird")
+else:
+    print("Not Weird")
+
+
+
+n = int(input("Enter a number: "))
+
+if (n in range (6, 21) or n % 2 != 0):
+    print("Weird")
+else:
+    print("Not Weird")
