15ef333c7dbd0c4886e495d5ad1203f1e8b794e4
Amoy N <moyanich@users.noreply.github.com>
2019-12-31 08:58:42 -0500

    Code added
    Code added

---
+++ conditionals.py
@@ -0,0 +1,14 @@
+# Given an integer, , perform the following conditional actions:
+
+# If  is odd, print Weird
+# If  is even and in the inclusive range of  to , print Not Weird
+# If  is even and in the inclusive range of  to , print Weird
+# If  is even and greater than , print Not Weird
+# Input Format
+
+# A single line containing a positive integer, n.
+
+n = input("Type word")
+
+if n=="Weird":
+    print("Weird")
