de419d96d810a3f976580c86d38009172d5da42f
Amoy Nicholson <moyanich@users.noreply.github.com>
2020-01-01 12:26:09 -0500

    Roman Numeral Equivalent
    For this exercise, start by creating a variable and assigning it a randomly generated integer between and inclusive of both 1 and 10.
    
    Completed using python dictionary function

---
+++ ex_7-roman-numeral-equivalent.py
@@ -0,0 +1,25 @@
+# Roman Numeral Equivalent
+# For this exercise, start by creating a variable and assigning it a randomly generated
+# integer between and inclusive of both 1 and 10.
+#
+# Then, using your knowledge of if, elif, and else statements, create a program which prints
+# the roman numeral equivalent of the randomly generated number.
+#
+# For example, if the randomly generated integer was 9, then the program would say that the
+# roman numeral equivalent of 9 is IX in the output.
+
+import random
+
+# generates random integer between and inclusive of 1 and 10
+random_number = random.randint(1, 10)
+
+# Used a dictionary to complete example.
+# Create the dictionary
+roman_numerals = {"i": 1, "ii": 2, "iii": 3, "iv":  4, "v": 5, "vi": 6, "vii": 7, "viii": 8,  "ix": 9, "x": 10}
+
+for numerals, num in roman_numerals.items():   # numerals - key, num - value
+    if num == random_number:
+        print(numerals)
+
+
+
