37813f8ef1bc64bb6d989367b0fb53e8dea86fdc
Amoy N <moyanich@users.noreply.github.com>
2020-01-03 12:08:51 -0500

    Create loops.py
    Loops example

---
+++ loops.py
@@ -0,0 +1,16 @@
+import math
+
+# Source: Hackerrank
+# link: https://www.hackerrank.com/challenges/python-loops/problem
+# Read an integer N . For all non-negative integers i < N, print i2.
+# Constraints
+# 1 <= N <= 20
+
+
+n = int(input())
+
+i = 0
+
+for i in range(n):
+    print(i)
+    # print(pow(i, 2))
